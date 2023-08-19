b7d2d4ef569ac169b5bc01a0040672b968a2e9bb
Drew Chase <dcmanproductions@gmail.com>
2023-08-19 13:33:22 -0400

    added boilerplate code

--- PhotoLab/App.xaml.cs
+++ PhotoLab/App.xaml.cs
@@ -1,12 +1,23 @@
-﻿namespace PhotoLab
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿// LFInteractive LLC. 2021-2024﻿
+namespace PhotoLab;
 
-            MainPage = new MainPage();
-        }
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+        window.Title = "PhotoLab - Open Source Photo Gallery";
+
+        window.MinimumWidth = 800;
+        window.MinimumHeight = 480;
+
+        return window;
     }
 }
--- PhotoLab/MauiProgram.cs
+++ PhotoLab/MauiProgram.cs
@@ -1,5 +1,6 @@
 ﻿// LFInteractive LLC. 2021-2024﻿
 using Microsoft.Extensions.Logging;
+using System.Reflection;
 
 namespace PhotoLab
 {
@@ -8,12 +9,7 @@ namespace PhotoLab
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
+            builder.UseMauiApp<App>();
 
             builder.Services.AddMauiBlazorWebView();
 
@@ -22,6 +18,10 @@ namespace PhotoLab
             builder.Logging.AddDebug();
 #endif
 
+            Task.Run(() =>
+            {
+                Chase.FFmpeg.Downloader.FFmpegDownloader.Instance.GetLatest(Directory.GetParent(Assembly.GetExecutingAssembly().Location).CreateSubdirectory("ffmpeg").FullName);
+            }).Wait();
             return builder.Build();
         }
     }
